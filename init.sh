47407950990eb5221bf6adfa123db145e0065c2e
Bill Mei <billmei@users.noreply.github.com>
2016-03-24 14:30:10 -0700

    Homebrew

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Initialization for development on MacOS
 
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Cask files
 
 brew cask install google-chrome
