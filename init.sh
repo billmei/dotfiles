d4bd8b5798d30315ddd22c01de838bcc6ecdf125
Bill Mei <billmei@users.noreply.github.com>
2016-05-23 19:48:11 -0700

    Custom OSX settings

--- init.sh
+++ init.sh
@@ -54,5 +54,92 @@ brew cask install font-comic-neue
 brew cask install font-montserrat
 
 # Custom settings
-# Disable two-finger swipe back/forward navigation in Chrome
+## Disable two-finger swipe back/forward navigation in Chrome
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
+
+## Speed up the green maximize window resize animation
+defaults write -g NSWindowResizeTime -float 0.001
+
+## Set the clock to 24h time
+defaults write -g AppleICUForce24HourTime -bool TRUE
+
+## Remove dock auto-hide animation delay
+defaults write com.apple.Dock autohide-delay -float 0
+
+## Make mission control animation super fast
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+## Make hidden apps translucent in the Dock
+defaults write com.apple.Dock showhidden -bool YES
+
+# From https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+
+## Disable transparency in the menu bar and elsewhere on Yosemite
+defaults write com.apple.universalaccess reduceTransparency -bool true
+
+## Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+## Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+## Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+## Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+
+## Trackpad: map bottom right corner to right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+## Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+## Set language and text formats
+## Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+## `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+defaults write NSGlobalDomain AppleLanguages -array "en" "ru" "es"
+defaults write NSGlobalDomain AppleLocale -string "en_US@currency=USD"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+## Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+## Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+## Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+## Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+## Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+## Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+## Always show hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles -bool YES
+
+## Always show the ~/Library folder
+chflags nohidden ~/Library
+
+## Always ahow the /Volumes folder
+sudo chflags nohidden /Volumes
+
+## Save all screenshots to ~/Pictures/Screenshots instead of the Desktop
+defaults write com.apple.screencapture location ~/Pictures/Screenshots
+
+## Refresh Dock and Finder to apply the above settings
+killall Dock
+killall Finder
