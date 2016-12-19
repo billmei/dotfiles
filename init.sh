19f99ef3d17f52593f5c93e72d48ddfaf4f6b8c7
Bill Mei <billmei@users.noreply.github.com>
2016-12-18 16:19:01 -0800

    Add more security options

--- init.sh
+++ init.sh
@@ -21,6 +21,10 @@ brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 
+# Security
+# Apple's default openssl is outdated
+brew install openssl
+
 # Cask files
 brew tap caskroom/cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
